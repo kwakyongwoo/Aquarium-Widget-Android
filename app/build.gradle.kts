979ee8afee648ecb143f0af3edcbe6ec569652f2
kwakyongwoo <kkolbuyw@gmail.com>
2025-03-11 16:15:13 +0900

    [fix] fix updateLastPlayDate & fishing when heart count is 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.dyddyd.aquariumwidget"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
